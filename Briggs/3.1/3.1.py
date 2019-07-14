191cb7950ffec4ea3dc290aa76c4e4108b81299f
LASHKOAG <LASHKOAG@yandex.ru>
2019-07-14 16:22:58 +0600

    work with list

--- Briggs/3.1/3.1.py
+++ Briggs/3.1/3.1.py
@@ -4,10 +4,18 @@ two
 three'''
 print(fred)
 print(fred2)
+print("\n")
 
 myscore=1000
 message='my score: %s check'
 print(message % myscore)
 print("mycore %s", myscore)
 print(message % 1000)
+print("\n")
 
+my_list=['one', 'two', 'three', 'four']
+print(my_list)
+print("\n")
+print(my_list[3])
+
+print(my_list[1:3])
