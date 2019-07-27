41ebda493e691ff9555b4e3efb728fbad40ba8a2
LASHKOAG <LASHKOAG@yandex.ru>
2019-07-27 22:46:02 +0600

    plot1 линейная диаграмма

---
+++ Briggs/6.1/6.1_loops.py
@@ -0,0 +1,12 @@
+for x in range(0,5):
+    print('привет')
+
+print("\n")
+print(list(range(10,20)))
+
+print("\n")
+wizard_list=['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten']
+for i in wizard_list:
+    print(i)
+
+print("\n")
