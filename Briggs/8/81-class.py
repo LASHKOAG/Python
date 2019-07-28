f3c683eabeed3ef0a7ec41585c6e31302afbd82a
LASHKOAG <LASHKOAG@yandex.ru>
2019-07-28 22:04:59 +0600

    добавлены фу-ии в классах - работа с классами

--- Briggs/8/81-class.py
+++ Briggs/8/81-class.py
@@ -13,11 +13,48 @@ class Sidewalks(Inanimate):
     pass
 
 class Animals(Animate):
-    pass
+    def breathe(self):
+        print("дышит\n")
+    def move(self):
+        print("передвигается\n")
+    def eat_food(self):
+        print("кушает\n")
+    
 
 class Mammals(Animals):
-    pass
-
+    def feed_young_with_milk(self):
+        print("кормит детенышей молоком\n")
+        
 class Giraffes(Mammals):
-    pass
+    def eat_leaves_from_trees(self):
+        print("кушает листья\n")
 
+
+reginald = Giraffes()
+reginald.move()
+reginald.eat_leaves_from_trees()
+
+harold = Giraffes()
+harold.move()
+
+import turtle
+avery = turtle.Pen()
+kate = turtle.Pen()
+
+avery.forward(50)
+avery.right(90)
+avery.forward(20)
+
+import time
+time.sleep (10)
+
+kate.left(90)
+kate.forward(100)
+
+time.sleep(10)
+
+jacob = turtle.Pen()
+jacob.left(180)
+jacob.forward(80)
+
+time.sleep(10)
